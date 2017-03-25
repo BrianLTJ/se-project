599e118f75a56d0b1b528b4e2cf4d91c800a7ceb
BrianLee <739142578@qq.com>
2017-03-25 19:43:18 +0800

    Book Models

--- book/models.py
+++ book/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__:
+        return name
+
+
+class Category(models.Model):
+    text = models.CharField(max_length=50)
+    note = models.TextField(null=True)
+
+    def __str__:
+        return text
+
+
+class Tag(models.Model):
+    text = models.CharField(max_length=50)
+    note = models.TextField(null=True)
+
+    def __str__:
+        return text
+
+
+class Book(models.Model):
+    bookid = models.AutoField(primary_key=TGrue)
+    cover = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    author = models.ManyToManyField(Author)
+    translator = models.ManyToManyField(Author)
+    edition = models.CharField(max_length=20)
+    pubhouse = models.CharField(max_length=50)
+    pubtime = models.CharField(max_length=20)
+    summary = models.TextField(null=True)
+    context = models.TextField(null=True)
+    price = models.
+    clc = models.CharField(max_length=20)
+
+    category = models.ManyToManyField(Category)
+    tag = models.ManyToManyField(Tag)
+
+    add_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+
+
+class LibBook(models.Model):
+    libbookid = models.AutoField(primary_key=True)
+    barid = models.CharField(max_length=20)
+    location = models.CharField(max_length=50)
+
