9ec2105a96928813a2faa5df975ae6f99220dd23
LiTingjun <739142578@qq.com>
2017-04-10 17:47:48 +0800

    Add functional tests, add templates

--- index/views.py
+++ index/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index_index(request):
     return render(request, template_name='index/index.html')
+
+
+def search_index(request):
+    return render(request, template_name='search/index.html')
