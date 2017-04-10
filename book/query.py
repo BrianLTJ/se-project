72fd6dfe9fc5d83a68d5f4909d805d2be5857152
BrianLee <739142578@qq.com>
2017-04-10 18:53:27 +0800

    add framework of query functions

---
+++ book/query.py
@@ -0,0 +1,18 @@
+'''
+Author: Tingjun Li
+Create Time: 2017-04-10
+Function: Find and show book details
+'''
+from book import models as book_model
+
+# List book
+def book_query_list(request):
+
+    return None
+
+
+# Show book details
+
+
+# Show lib book details
+
