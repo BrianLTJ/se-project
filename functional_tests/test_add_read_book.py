60a668d4729b692f051b0fe5eb22f73609c35c4e
LiTingjun <739142578@qq.com>
2017-04-15 01:17:26 +0800

    functional test

--- functional_tests/test_add_read_book.py
+++ functional_tests/test_add_read_book.py
@@ -15,18 +15,17 @@ class AddBookTest(StaticLiveServerTestCase):
     testadd = [["isbn", "9787505715660"], ["title", "小王子"], ["author", "（法）圣埃克苏佩里"], ["translator", "胡雨苏"],["edition", "13014"], ["pubhouse", "中国友谊出版公司"], ["pubtime", "12342253"],["summary", "小王子驾到！大家好，我是小王子，生活在B612星球，别看我是王子出生，我要做的事也不少，有时给花浇水，有时我还得耐心地把火山口通一通。实在闷得发慌的时候，为了找些事做"],["context", "序言：法兰西玫瑰 小王子 圣埃克苏佩里年表"], ["clc", "239/6"], ["price", "11.4"]]
     catelist1 = ["aaa","bbb","ccc","252fd","测试分类1","计算机","金融","医学","电子"]
     catelist2 = ["fff","ggg","jjj","afw","g2rg2"]
-    taglist =["BHpmw","MNIoT","va0crc","9w3pgs","ZnkNH","XXrcB","ZANkuR","1xTD","0Daf","pGyleT","lAD9","s6yAXa","22RC4s","SrZ6Ab","v3Fp9","XtwR3","W87Xd","NYAi0","GrvlN","typM","T96Ol","z1gg","TWqmR5","JrIh","gIfn","RrCU","r4jx"]
+    taglist =["BHpmw","MNIoT","va0crc","ZnkNH1xTD","pGyleT","lAD9","s6yAXa","GrvlN","typM","T96Ol","z1gg","TWqmR5"]
 
     def setUp(self):
         self.browser = webdriver.PhantomJS()
         # self.browser = webdriver.Chrome()
 
-
     def tearDown(self):
         self.browser.quit()
 
 
-    def test_add_book_with_cate(self):
+    def test_add_book(self):
         # Add book
         self.browser.get(self.live_server_url+'/admin/book/add')
 
@@ -60,13 +59,13 @@ class AddBookTest(StaticLiveServerTestCase):
             new_tag_add_btn.click()
             time.sleep(0.3)
 
-
         submitbtn = self.browser.find_element_by_id('btn-save')
         submitbtn.click()
+        time.sleep(2)
 
         # book detail
         self.browser.get(self.live_server_url+'/book/detail/1')
-        time.sleep(0.5)
+        time.sleep(1)
 
         # Test Book info
         readbookinfo = self.browser.find_element_by_id('book-info').text
