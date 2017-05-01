0341bf2a21ea6969a139710493e48b5357c92d96
LiTingjun <739142578@qq.com>
2017-05-01 23:40:48 +0800

    BookBorrow

--- user/models.py
+++ user/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import Group, User
 
 # Create your models here.
 class BorrowRight(models.Model):
+    name = models.CharField(null=True, blank=True)
     booknum = models.IntegerField(default=0)
     day = models.IntegerField(default=0)
     allowborrow = models.BooleanField(default=False)
